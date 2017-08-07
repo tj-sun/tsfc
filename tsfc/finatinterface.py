32a3dfa9488069a7e6d9d8c5f58e2a9732cef3b5
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-08-07 14:36:12 +0100

    register RuntimeTabulated elements in the FInAT interface

--- tsfc/finatinterface.py
+++ tsfc/finatinterface.py
@@ -92,8 +92,8 @@ def convert(element, **kwargs):
 @convert.register(ufl.FiniteElement)
 def convert_finiteelement(element, **kwargs):
     cell = as_fiat_cell(element.cell())
+    degree = element.degree()
     if element.family() == "Quadrature":
-        degree = element.degree()
         scheme = element.quadrature_scheme()
         if degree is None or scheme is None:
             raise ValueError("Quadrature scheme and degree must be specified!")
@@ -118,6 +118,11 @@ def convert_finiteelement(element, **kwargs):
             lmbda = finat.Lagrange
         elif kind == 'spectral' and element.cell().cellname() == 'interval':
             lmbda = finat.GaussLobattoLegendre
+        elif kind in ['mgd', 'feec']:
+            shift_axes = kwargs["shift_axes"]
+            restriction = kwargs["restriction"]
+            deps = {"shift_axes", "restriction"}
+            return finat.RuntimeTabulated(cell, degree, kind, shift_axes, restriction), deps
         else:
             raise ValueError("Variant %r not supported on %s" % (kind, element.cell()))
     elif element.family() == "Discontinuous Lagrange":
@@ -126,9 +131,14 @@ def convert_finiteelement(element, **kwargs):
             lmbda = finat.DiscontinuousLagrange
         elif kind == 'spectral' and element.cell().cellname() == 'interval':
             lmbda = finat.GaussLegendre
+        elif kind in ['mgd', 'feec']:
+            shift_axes = kwargs["shift_axes"]
+            restriction = kwargs["restriction"]
+            deps = {"shift_axes", "restriction"}
+            return finat.RuntimeTabulated(cell, degree, kind, shift_axes, restriction, continuous=False), deps
         else:
             raise ValueError("Variant %r not supported on %s" % (kind, element.cell()))
-    return lmbda(cell, element.degree()), set()
+    return lmbda(cell, degree), set()
 
 
 # Element modifiers and compound element types
@@ -175,9 +185,20 @@ def convert_tensorproductelement(element, **kwargs):
     cell = element.cell()
     if type(cell) is not ufl.TensorProductCell:
         raise ValueError("TensorProductElement not on TensorProductCell?")
-    elements, deps = zip(*[_create_element(elem, **kwargs)
-                           for elem in element.sub_elements()])
-    return finat.TensorProductElement(elements), set.union(*deps)
+
+    shift_axes = kwargs["shift_axes"]
+    dim_offset = 0
+
+    elements = []
+    deps = set()
+    for elem in element.sub_elements():
+        kwargs["shift_axes"] = shift_axes + dim_offset
+        dim_offset += elem.cell().topological_dimension()
+
+        finat_elem, ds = _create_element(elem, **kwargs)
+        elements.append(finat_elem)
+        deps.update(ds)
+    return finat.TensorProductElement(elements), deps
 
 
 @convert.register(ufl.HDivElement)
@@ -202,14 +223,20 @@ quad_tpc = ufl.TensorProductCell(ufl.interval, ufl.interval)
 _cache = weakref.WeakKeyDictionary()
 
 
-def create_element(ufl_element, shape_innermost=True):
+def create_element(ufl_element, shape_innermost=True, shift_axes=0, restriction=None):
     """Create a FInAT element (suitable for tabulating with) given a UFL element.
 
     :arg ufl_element: The UFL element to create a FInAT element from.
     :arg shape_innermost: Vector/tensor indices come after basis function indices
+    :arg shift_axes: shift axes for runtime tabulated elements inside
+                     tensor product elements
+    :arg restriction: cell restriction in interior facet integrals
+                      (only for runtime tabulated elements)
     """
     finat_element, deps = _create_element(ufl_element,
-                                          shape_innermost=shape_innermost)
+                                          shape_innermost=shape_innermost,
+                                          shift_axes=shift_axes,
+                                          restriction=restriction)
     return finat_element
 
 
